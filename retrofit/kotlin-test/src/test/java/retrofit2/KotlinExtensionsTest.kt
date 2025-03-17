0ccb62839106d236a85104d3b9fdfd27408e6cbf
Pooja-8112003 <pooja.m.intern@lightmetrics.co>
2025-03-17 12:03:32 +0530

    added the changes to kotlin file

--- retrofit/kotlin-test/src/test/java/retrofit2/KotlinExtensionsTest.kt
+++ retrofit/kotlin-test/src/test/java/retrofit2/KotlinExtensionsTest.kt
@@ -33,3 +33,12 @@ class KotlinExtensionsTest {
     assertNotNull(retrofit.create<Empty>())
   }
 }
+fun main(args: Array<String>) {
+
+    val first: Int = 10
+    val second: Int = 20
+
+    val sum = first + second
+
+    println("The sum is: $sum")
+}
