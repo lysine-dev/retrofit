563e13faabdf24b91e684847afaf7aff9d0e71b7
Pooja-8112003 <pooja.m.intern@lightmetrics.co>
2025-03-18 11:57:02 +0530

    added the line in extension file

--- retrofit/kotlin-test/src/test/java/retrofit2/KotlinExtensionsTest.kt
+++ retrofit/kotlin-test/src/test/java/retrofit2/KotlinExtensionsTest.kt
@@ -33,13 +33,7 @@ class KotlinExtensionsTest {
     assertNotNull(retrofit.create<Empty>())
   }
 
-fun main(args: Array<String>) {
-
-    val first: Int = 10
-    val second: Int = 20
-
-    val sum = first + second
-
-    println("The sum is: $sum")
+{
+println("hello world")
 }
 }
