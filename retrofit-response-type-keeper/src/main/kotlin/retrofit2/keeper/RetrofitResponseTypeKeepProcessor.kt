ce4bb50e10fcc6fbb838249fc18347eda394b535
Goooler <wangzongler@gmail.com>
2025-11-06 13:11:44 +0800

    Eliminate KAPT usage

--- retrofit-response-type-keeper/src/main/kotlin/retrofit2/keeper/RetrofitResponseTypeKeepProcessor.kt
+++ retrofit-response-type-keeper/src/main/kotlin/retrofit2/keeper/RetrofitResponseTypeKeepProcessor.kt
@@ -15,9 +15,7 @@
  */
 package retrofit2.keeper
 
-import com.google.auto.service.AutoService
 import javax.annotation.processing.AbstractProcessor
-import javax.annotation.processing.Processor
 import javax.annotation.processing.RoundEnvironment
 import javax.lang.model.SourceVersion
 import javax.lang.model.element.ExecutableElement
@@ -26,11 +24,7 @@ import javax.lang.model.type.DeclaredType
 import javax.lang.model.type.TypeMirror
 import javax.lang.model.type.WildcardType
 import javax.tools.StandardLocation.CLASS_OUTPUT
-import net.ltgt.gradle.incap.IncrementalAnnotationProcessor
-import net.ltgt.gradle.incap.IncrementalAnnotationProcessorType.ISOLATING
 
-@AutoService(Processor::class)
-@IncrementalAnnotationProcessor(ISOLATING)
 class RetrofitResponseTypeKeepProcessor : AbstractProcessor() {
   override fun getSupportedSourceVersion() = SourceVersion.latestSupported()
   override fun getSupportedAnnotationTypes() = setOf(
